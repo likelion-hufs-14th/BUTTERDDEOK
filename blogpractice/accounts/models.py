149c89747bd2ecdae175dbffd6538bbc3f258ccb
angjihyun <jihyunyang218@gmail.com>
2026-05-11 15:43:02 +0900

    blogpracticeworkk

---
+++ blogpractice/accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+  nickname = models.CharField(max_length=100)
+  university = models.CharField(max_length=50)
+  location = models.CharField(max_length=200)
